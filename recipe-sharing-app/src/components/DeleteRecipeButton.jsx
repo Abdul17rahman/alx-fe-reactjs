21cf3c144c573347d35ff9bf24f762f4cdf25e0a
Abdul Rahman <abdulnsamba@gmail.com>
2025-03-02 16:07:54 +0300

    del and update --fix--

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 function DeleteRecipeButton({ recipeId }) {
   const { deleteRecipe } = useRecipeStore();
 
-  return <button onClick={deleteRecipe(recipeId)}>Delete</button>;
+  const navigate = useNavigate();
+
+  function handleDel() {
+    deleteRecipe(recipeId);
+    navigate("/");
+  }
+
+  return <button onClick={handleDel}>Delete</button>;
 }
 
 export default DeleteRecipeButton;
