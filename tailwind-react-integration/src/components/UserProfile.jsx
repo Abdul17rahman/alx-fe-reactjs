d2eda173972627731bfd558280b6d1dabb81abaf
Abdul Rahman <abdulnsamba@gmail.com>
2025-03-12 13:00:45 +0300

    fea: responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg md:p-8 sm:p-4 md:max-w-xs md:max-w-s">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg md:p-8 sm:p-4 max-w-xs md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
